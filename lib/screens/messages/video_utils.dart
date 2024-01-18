44087dbbf3903ae5547916092d84c9cd07a0ca90
theotherdy <allotmentor@allotmentor.com>
2024-01-18 22:48:05 +0000

    Starting landscape dimensions

--- lib/screens/messages/video_utils.dart
+++ lib/screens/messages/video_utils.dart
@@ -10,11 +10,17 @@ class VideoUtils {
     double videoWidth = min(maxWidth, controller.value.size.width);
     double videoHeight = min(maxHeight, videoWidth / aspectRatio);
 
+    bool heightLimiting = false;
+    bool widthLimiting = false;
+
     // If the video is in portrait mode, reduce the width further to ensure the height fits within the rectangle
     if (controller.value.size.aspectRatio < 1.0) {
       videoWidth = min(videoWidth, videoHeight * aspectRatio);
       videoHeight = videoWidth / aspectRatio;
     }
+
+    if(videoWidth > maxWidth) heightLimiting = true;
+    if(videoHeight > maxHeight) widthLimiting = true;
 
     return Size(videoWidth, videoHeight);
   }
